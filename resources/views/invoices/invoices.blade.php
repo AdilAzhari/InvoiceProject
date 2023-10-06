d94e30532514bc10633a2f17f55e7654c78cd2bf
Adil omer <36453919+adelazhary@users.noreply.github.com>
2023-10-06 22:09:58 +0800

    invoice layout

--- resources/views/invoices/invoices.blade.php
+++ resources/views/invoices/invoices.blade.php
@@ -1,12 +1,20 @@
 @extends('layouts.master')
+
 @section('css')
+<link href="{{URL::asset('assets/plugins/datatable/css/dataTables.bootstrap4.min.css')}}" rel="stylesheet" />
+<link href="{{URL::asset('assets/plugins/datatable/css/buttons.bootstrap4.min.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/plugins/datatable/css/responsive.bootstrap4.min.css')}}" rel="stylesheet" />
+<link href="{{URL::asset('assets/plugins/datatable/css/jquery.dataTables.min.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/plugins/datatable/css/responsive.dataTables.min.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/plugins/select2/css/select2.min.css')}}" rel="stylesheet">
 @endsection
+
 @section('page-header')
 				<!-- breadcrumb -->
 				<div class="breadcrumb-header justify-content-between">
 					<div class="my-auto">
 						<div class="d-flex">
-							<h4 class="content-title mb-0 my-auto">Pages</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ Empty</span>
+							<h4 class="content-title mb-0 my-auto">Tables</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ Data Tables</span>
 						</div>
 					</div>
 					<div class="d-flex my-xl-auto right-content">
@@ -38,15 +46,85 @@
 				<!-- breadcrumb -->
 @endsection
 @section('content')
-				<!-- row -->
-				<div class="row">
+				<!-- row opened -->
+				<div class="row row-sm">
+					<!--div-->
+					<div class="col-xl-12">
+						<div class="card mg-b-20">
+							<div class="card-header pb-0">
+								<div class="d-flex justify-content-between">
+									<h4 class="card-title mg-b-0">Bordered Table</h4>
+									<i class="mdi mdi-dots-horizontal text-gray"></i>
+								</div>
+								<p class="tx-12 tx-gray-500 mb-2">Example of Valex Bordered Table.. <a href="">Learn more</a></p>
+							</div>
+							<div class="card-body">
+								<div class="table-responsive">
+									<table id="example" class="table key-buttons text-md-nowrap">
+										<thead>
+											<tr>
+												<th class="border-bottom-0">#</th>
+												<th class="border-bottom-0">Invoice Number</th>
+												<th class="border-bottom-0">Invoice Date</th>
+												<th class="border-bottom-0">Due Date</th>
+												<th class="border-bottom-0">Products</th>
+												<th class="border-bottom-0">Section</th>
+												<th class="border-bottom-0">Discount</th>
+												<th class="border-bottom-0">rate_vat</th>
+												<th class="border-bottom-0">Value_vat</th>
+                                                <th class="border-bottom-0">Total</th>
+                                                <th class="border-bottom-0">Status</th>
+												<th class="border-bottom-0">Note</th>
 
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+                                                <td>1</td>
+												<td>Donna Snider</td>
+												<td>Customer Support</td>
+												<td>New York</td>
+												<td>27</td>
+												<td>2011/01/25</td>
+												<td>$112,000</td>
+												<td>Customer Support</td>
+												<td>New York</td>
+												<td>27</td>
+												<td>2011/01/25</td>
+												<td>$112,000</td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!--/div-->
 				</div>
-				<!-- row closed -->
+				<!-- /row -->
 			</div>
 			<!-- Container closed -->
 		</div>
 		<!-- main-content closed -->
 @endsection
 @section('js')
+<!-- Internal Data tables -->
+<script src="{{URL::asset('assets/plugins/datatable/js/jquery.dataTables.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.dataTables.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/responsive.dataTables.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/jquery.dataTables.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.bootstrap4.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.buttons.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/buttons.bootstrap4.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/jszip.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/pdfmake.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/vfs_fonts.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/buttons.html5.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/buttons.print.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/buttons.colVis.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/responsive.bootstrap4.min.js')}}"></script>
+<!--Internal  Datatable js -->
+<script src="{{URL::asset('assets/js/table-data.js')}}"></script>
 @endsection
